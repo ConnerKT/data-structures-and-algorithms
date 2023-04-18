10397eee10aa416585fa95f5759b01a59fa395b7
ConnerKT <connerthompson121@gmail.com>
2023-04-17 23:20:01 -0500

    solution for uppercase

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -48,6 +48,12 @@ Use `forEach` to loop over the input array. The modified strings should each be 
 
 const allUpperCase = (arr) => {
   // Solution code here...
+  let arrayUpper = [];
+  arr.forEach(element => {
+    arrayUpper.push(element.toUpperCase());
+
+  });
+  return arrayUpper;
 };
 
 /* ------------------------------------------------------------------------------------------------
