ecc16699efd1ccf9646a61c969c952f412c0dc41
ConnerKT <connerthompson121@gmail.com>
2023-04-20 20:47:58 -0500

    challenge 2 solution

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -25,6 +25,10 @@ For example, typeNum([1, 'bob' ,3]) returns [1,3].
 
 const typeNum = (arr) => {
   // Solution code here...
+  let newArr = arr.filter(n => {
+    return (n > 0)
+  })
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
