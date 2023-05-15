9d88eb2cad8d549beb32233460745f039872f120
ConnerKT <connerthompson121@gmail.com>
2023-05-15 18:49:22 -0500

    pushing questions 1-4

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -3,18 +3,22 @@
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 1 - Review
 
-Write a function named returnTen, takes in a string and uses split and splice to return the last 10 characters from that string as elements of an array.
+Write a function named returnTen, takes in a string and uses split and splice
+ to return the last 10 characters from that string as elements of an array.
 
 ------------------------------------------------------------------------------------------------ */
 
 function returnTen(str){
   // Solution code here...
+  //Makes an array with each character in the str split
+return str.split('').splice(-10)
 }
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
 
-Write a function named findMax that takes in a matrix of positive numbers and returns the number with the highest value.
+Write a function named findMax that takes in a matrix of positive
+numbers and returns the number with the highest value.
 
 For example:
 [
@@ -27,12 +31,25 @@ return: 23
 ------------------------------------------------------------------------------------------------ */
 const findMax = (matrix) => {
   // Solution code here...
+  let highestNumber = 0;
+  for (let i = 0; i < matrix.length; i++){
+    for (let x = 0; x < matrix[i].length; x++){
+      let current = matrix[i][x]
+      if (current > highestNumber){
+        highestNumber = current
+      }else {
+        highestNumber + 0
+      }
+    }
+  }
+  return highestNumber
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
 
-Write a function named totalSum that takes in a matrix of numbers and returns the totalSum of all the numbers.
+Write a function named totalSum that takes in a matrix of numbers
+and returns the totalSum of all the numbers.
 
 For example:
 [
@@ -45,19 +62,35 @@ return: 35
 ------------------------------------------------------------------------------------------------ */
 const totalSum = (matrix) => {
   // Solution code here...
+  let total = 0;
+  for (let i = 0; i < matrix.length; i++){
+    for (let x = 0; x < matrix[i].length; x++){
+      let current = matrix[i][x]
+      total += current
+    }
+  }
+  return total
 };
 
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
 
-You friend Pat has a chain of stores around the greater Seattle area. He specializes in selling salmon cookies. Pat has data for the hourly sales of cookies per hour for each store. He wants to create an array of the total number of cookies sold per hour for all of his stores combined.
+You friend Pat has a chain of stores around the greater Seattle area.
+He specializes in selling salmon cookies. Pat has data for the hourly sales
+ of cookies per hour for each store. He wants to create an array of the total
+ number of cookies sold per hour for all of his stores combined.
 
-Write a function named grandTotal that adds up the cookies sales for each hour of operation for all of the stores combined. For example, the first element in the hourlySales array should be the sum of the cookies sold in the 9:00 a.m. hour at all five stores combined.
+Write a function named grandTotal that adds up the cookies sales for each hour
+of operation for all of the stores combined. For example, the first element in
+the hourlySales array should be the sum of the cookies sold in the 9:00 a.m. hour
+at all five stores combined.
 
-For this example, the total at 9:00 a.m. is 17 + 26 + 7 + 5 + 33, or 88 total cookies.
+For this example, the total at 9:00 a.m. is 17 + 26 + 7 + 5 + 33, or 88 total
+cookies.
 
-Return the array of the total number of cookies sold per hour for all of the stores combined.
+Return the array of the total number of cookies sold per hour for all of the
+stores combined.
 ------------------------------------------------------------------------------------------------ */
 
 const hoursOpen = ['9 a.m.', '10 a.m.', '11 a.m.', '12 p.m.', '1 p.m.', '2 p.m.', '3 p.m.', '4 p.m.', '5 p.m.', '6 p.m.', '7 p.m.', '8 p.m.'];
@@ -72,17 +105,23 @@ const cookieStores = [firstPike, seaTac, seattleCenter, capHill, alkiBeach];
 
 const grandTotal = (stores) => {
   // Solution code here...
-
+  let newArr = [];
+for(let x=0; x < stores.length; x++){
+  
+}
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
 
-Pat has decided that he would also like to organize his data as objects containing the number of cookies sold per hour and the time.
+Pat has decided that he would also like to organize his data as objects
+containing the number of cookies sold per hour and the time.
 
 Here is sample data for the 9:00 sales: { sales: '88 cookies', time: '9 a.m.' }.
 
-Write a function named salesData that uses forEach to iterate over the hourlySales array and create an object for each hour. Return an array of the formatted data.
+Write a function named salesData that uses forEach to iterate over the
+hourlySales array and create an object for each hour. Return an array of
+the formatted data.
 ------------------------------------------------------------------------------------------------ */
 
 const salesData = (hours, data) => {
@@ -92,7 +131,9 @@ const salesData = (hours, data) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
 
-Write a function named howManyTreats that will return the quantity of treats you need to pick up from the pet store today from this array. The structure of the array will not change.
+Write a function named howManyTreats that will return the quantity of
+treats you need to pick up from the pet store today from this array.
+The structure of the array will not change.
 ------------------------------------------------------------------------------------------------ */
 
 const errands = [
@@ -117,9 +158,12 @@ const howManyTreats = (arr) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
 
-Write a function named battleship that accepts a 2D array and two numbers: a row coordinate and a column coordinate.
+Write a function named battleship that accepts a 2D array and two numbers:
+a row coordinate and a column coordinate.
 
-Return "hit" or "miss" depending on if there's part of a boat at that position in the array. Assume the array has only one of two values at each index. '#' for part of a boat, or ' ' for open water.
+Return "hit" or "miss" depending on if there's part of a boat at that position
+in the array. Assume the array has only one of two values at each index. '#'
+for part of a boat, or ' ' for open water.
 
 Here is a sample board:
 [
@@ -129,7 +173,8 @@ Here is a sample board:
   [' ', ' ', '#', '#'],
 ]
 
-The top row of the board is considered row zero and row numbers increase as they go down.
+The top row of the board is considered row zero and row numbers increase as
+they go down.
 ------------------------------------------------------------------------------------------------ */
 
 const battleship = (board, row, col) => {
@@ -139,7 +184,9 @@ const battleship = (board, row, col) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 8 - Stretch Goal
 
-Write a function named calculateProduct that takes in a two-dimensional array of numbers, multiplies all of the numbers in each array, and returns the final product. This function should work for any number of inner arrays.
+Write a function named calculateProduct that takes in a two-dimensional
+array of numbers, multiplies all of the numbers in each array, and returns
+the final product. This function should work for any number of inner arrays.
 
 For example, the following input returns a product of 720: [[1,2], [3,4], [5,6]]
 ------------------------------------------------------------------------------------------------ */
@@ -151,9 +198,12 @@ const calculateProduct = (numbers) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 9 - Stretch Goal
 
-Write a function named averageDailyTemperature that accepts a two-dimensional array representing average daily temperatures grouped week-by-week.
+Write a function named averageDailyTemperature that accepts a two-dimensional
+array representing average daily temperatures grouped week-by-week.
 
-Calculate the average daily temperature during that entire period. Your output should be a single number. Write your function so it could accept an array with any number of weeks given to it.
+Calculate the average daily temperature during that entire period. Your output
+should be a single number. Write your function so it could accept an array with
+ any number of weeks given to it.
 ------------------------------------------------------------------------------------------------ */
 
 // Real daily average temperatures for Seattle, October 1-28 2017
@@ -171,11 +221,15 @@ const averageDailyTemperature = (weather) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 10 - Stretch Goal
 
-Write a function named lowestWeeklyAverage that accepts a two-dimensional array of daily temperatures grouped week-by-week.
+Write a function named lowestWeeklyAverage that accepts a two-dimensional
+array of daily temperatures grouped week-by-week.
 
-Calculate the average temperature for each week and return the value of the lowest weekly average temperature.
+Calculate the average temperature for each week and return the value of the
+lowest weekly average temperature.
 
-For example, in the data set below, the lowest weekly average is 46, which is the average of the temperatures in week 2. All other weeks have average temperatures that are greater than 46.
+For example, in the data set below, the lowest weekly average is 46, which is
+the average of the temperatures in week 2. All other weeks have average
+temperatures that are greater than 46.
 ------------------------------------------------------------------------------------------------ */
 
 let lowestWeeklyTemperatureData = [
